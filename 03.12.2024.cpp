5a7061b713eaff5e5d1276784b0995b93fb6e78a
AbsGosha <159914520+AbsGosha@users.noreply.github.com>
2024-12-03 21:18:47 +0300

    Add files via upload

---
+++ 03.12.2024.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "List.h"
+using namespace std;
+
+int main()
+{
+	List <int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	cout << endl;
+	list.print();
+	cout << endl;
+	list.push_front(1);
+	list.push_front(2);
+	list.push_front(3);
+	cout << endl;
+	list.print();
+	cout << endl;
+	list.delete_last();
+	cout << endl;
+	list.print();
+	cout << endl;
+	list.delete_first();
+	cout << endl;
+	list.print();
+}
---
+++ List.h
@@ -0,0 +1,76 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template <typename T>
+struct Node
+{
+	T value;
+	Node* next;
+
+	Node(T value) : value(value), next(nullptr) {};
+};
+
+template <typename T>
+class List
+{
+private:
+	Node<T>* first;
+public:
+	List() : first(nullptr){}
+	void push_back(T value) {
+		if (this->empty()) {
+			this->first = new Node<T>(value);
+				return;
+		}
+		Node<T>* p = this->first;
+		while (p->next != nullptr) {
+			p = p->next;
+		}
+		p->next = new Node<T>(value);
+	}
+	bool empty() { return (first == nullptr) ? true : false; }
+
+	void print() {
+		if (this->empty()) {
+			return;
+		}
+		Node<T>* p = this->first;
+		while (p != nullptr) {
+			cout << p->value << " ";
+			p = p->next;
+		}
+		cout << endl;
+	}
+	void push_front(T value) { // добавление элемента в начало
+		if (this->empty()) {
+			this->first = new Node<T>(value);
+			return;
+		}
+		Node<T>* p = new Node<T>(value);
+		p->next = this->first;
+		this->first = p;
+	}
+	void delete_last() {
+		if (this->empty()) {
+			return;
+		}
+		Node<T>* p = first;
+		while (p->next->next != nullptr) {
+			p = p->next;
+		}
+		delete p->next;
+		p->next = nullptr;
+
+	}
+	void delete_first() {
+		if (this->empty()) {
+			return;
+		}
+		Node<T>* p = first;
+		first = p->next;
+		delete p;
+
+	}
+};
+
